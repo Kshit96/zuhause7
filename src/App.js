999fd0b230578f55d2db4b210aaafb8241af990a
Kshit96 <kshit96@gmail.com>
2021-08-01 18:48:45 +0530

    Add elements of home page like logo and text with bg image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import BG from './images/LandingPageBG.png';
 import './App.css';
 import { Row, Col } from 'antd';
+import { Button, DatePicker, version } from "antd";
 import styled from 'styled-components';
 import { ReactComponent as Logo } from './images/ZuHause_Logo_Complete_Light.svg';
 
@@ -12,20 +13,41 @@ background-image: url(${BG});
 background-size: cover;
 `;
 
-const CenteredRow = styled(Row)`
+const CenteredCol = styled(Col)`
+display: flex;
 align-items: center;
 justify-content: center;
 `
 
+const BoldP = styled.p`
+color: #A99060;
+font-family: AvenirNextBold;
+width: fit-content;
+margin: 0!important;
+display: inline;
+`
+
 const App = () => {
   return (
-    <StyledRow>
+    <StyledRow justify="center" align="middle">
       <Col>
-        <CenteredRow>
-          <Col span={12} offset={6}>
-            <Logo />
-          </Col>
-        </CenteredRow>
+        <Row>
+          <CenteredCol span={4} offset={10}>
+            <Logo style={{ alignSelf: 'flex-end' }} />
+          </CenteredCol>
+        </Row>
+        <Row>
+          <CenteredCol style={{ textAlign: 'center', color: '#FCFEFB', fontSize: '20px', display: 'inline' }} span={18} offset={3}>
+            <br />
+            Zu•hau•se  / [tsuˈhauzə] /, meaning <BoldP>at home</BoldP>, is a dream put to implementation by <BoldP>Zahra Chadha.</BoldP>
+            <br /><br />
+            Our aim is to ensure you get what you want to make your spaces as <BoldP>functional, comfortable and warm</BoldP> as possible.<br/>
+            Let your dreams of getting that specific lighting, furniture, signature art piece, flooring or rugs be converted into reality by Zuhause7.<br/>
+            We get the best, and more importantly what you need, from the Europe and the USA. And if that does not satiate your desire for your dream space,
+            we can help you source it locally to your bespoke requirements as well.
+            <br /><br /><br /><br /><br />
+          </CenteredCol>
+        </Row>
       </Col>
     </StyledRow>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "antd/dist/antd.css";
+import './fonts/AvenirNext-Bold.ttf';
+import './fonts/AvenirNext-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
