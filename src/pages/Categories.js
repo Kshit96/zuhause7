9db8a58aabbea7a6a9fd195e9912ae092d3a72da
Kshit96 <kshit96@gmail.com>
2021-08-04 00:00:35 +0530

    Add hover behaviour for category images

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -13,11 +13,12 @@ background-size: cover;
 `
 const FullWidthRow = styled(Row)`
 width: 100%;
+overflow: hidden;
 `
 
 const HeadingP = styled.p`
 font-family: AvenirNextBold;
-font-size: 40px;
+font-size: 35px;
 color: #FCFEFB;
 margin: 0;
 `
@@ -27,28 +28,76 @@ width: 20rem;
 margin-top: 1rem;
 `
 
-const ImageCol = styled(Col)`
+const ImageCol = styled.div`
+width: 30rem;
 height: 25rem;
 overflow: hidden;
 display: flex;
+position: relative;
 align-items: center;
 justify-content: center;
 border-radius: 1rem;
+transform-origin: center;
+transition: height 0.5s, width 0.5s;
 .ant-image{
     position: absolute!important;
 }
+&:hover{
+    transition: height 0.5s, width 0.5s;
+    transform-origin: center;
+    height: 30rem;
+    cursor: pointer;
+    width: 35rem!Important;
+}
+
+img{
+    transform-origin: center;
+    width: 32rem;
+    align-self: center;
+    transition: width 0.5s;
+}
+
+span{
+    transition: top 0.5s;
+    z-index:3;
+    position: relative!Important;
+    top: 500px;
+    width: 100%;
+    text-align: center;
+}
+
+p{
+    margin-left: 0rem;
+    transition: margin-left 0.5s;
+}
+
+&:hover p{
+    transition: margin-left 1s;
+    margin-left: -20rem;
+}
+
+&:hover span{
+    position: relative;
+    top: 215px;
+    transition: top 0.5s;
+}
+
+&:hover img{
+    transform-origin: center;
+    transition: width 0.5s;
+    width: 38rem;
+}
+
 `
 
-const StyledImage = styled(Image)`
-width: 32rem;
-align-self: center;
-
+const StyledSpan = styled.span`
+background: rgba(15, 18, 26, 0.9);
 `
 
 const Categories = () => {
     return (
         <StyledRowWithBG>
-            <FullWidthRow>
+            <FullWidthRow style={{ height: '40%' }}>
                 <CenteredCol style={{ flexDirection: 'column' }} span={24}>
                     <HeadingP>Explore Now</HeadingP>
                     <StyledHR />
@@ -60,18 +109,18 @@ const Categories = () => {
                     </IntroductionCol>
                 </CenteredCol>
             </FullWidthRow>
-            <FullWidthRow justify='space-around'>
+            <FullWidthRow justify='space-around' style={{ height: '60%' }}>
                 <ImageCol span={6}>
-                    <StyledImage preview={false} src={Furniture} />
-                    <p style={{zIndex: 3}}>Furniture</p>
+                    <Image preview={false} src={Furniture} />
+                    <StyledSpan><HeadingP>FURNITURE</HeadingP></StyledSpan>
                 </ImageCol>
                 <ImageCol span={6}>
-                <StyledImage preview={false} src={Lighting} />
-                    <p style={{zIndex: 3}}>Lighting</p>
+                    <Image preview={false} src={Lighting} />
+                    <StyledSpan><HeadingP>LIGHTING</HeadingP></StyledSpan>
                 </ImageCol>
                 <ImageCol span={6}>
-                <StyledImage preview={false} src={Artefacts} />
-                    <p style={{zIndex: 3}}>Artefacts</p>
+                    <Image preview={false} src={Artefacts} />
+                    <StyledSpan><HeadingP>ARTEFACTS</HeadingP></StyledSpan>
                 </ImageCol>
             </FullWidthRow>
         </StyledRowWithBG>
